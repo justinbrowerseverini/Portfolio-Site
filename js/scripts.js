c0d00e50edf7a92b70949ed20eeaf4813570ac56
Justin Brower-Severini <jseverini.c@gmail.com>
2019-10-15 15:24:23 -0400

    added interactive menu for mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Hey there! Wellcome to my Portfolio Site!");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
